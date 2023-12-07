2948a8fef7818c60ec1cf0a3d0c02fde6b7d92d2
Ashutosh Rai <151700087+ashusomehow@users.noreply.github.com>
2023-12-07 19:16:56 +0530

    Create PrimeNumberChecker.c

---
+++ PrimeNumberChecker.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int num) {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int num;
+
+    // Get user input
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check and print whether the number is prime
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
